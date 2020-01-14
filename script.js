e9faf1e4c783915700cee0642d9af5a9f527ac28
aronzych <57272508+aronzych@users.noreply.github.com>
2020-01-14 18:55:12 +0100

    fix media, add new animation scale, prefixes

--- script.js
+++ script.js
@@ -8,32 +8,32 @@ const openAbout = document.querySelector(".main__button--openAbout");
 const closePortfolio = document.querySelector(".portfolio__button");
 const closeAbout = document.querySelector(".about__button");
 
-const showPortfolio = function() {
-    main.style.transitionDelay="0s";
-    main.style.transform="scale(0.75)"
-    portfolio.style.transitionDelay=".3s";
-    portfolio.style.transform="translateX(0%)"
+const showPortfolio = function () {
+    main.style.transitionDelay = "0s";
+    main.style.transform = "scale(0.75)";
+    portfolio.style.transitionDelay = ".3s";
+    portfolio.style.transform = "scale(1)";
 };
-const showAbout = function() {
-    
-    main.style.transitionDelay="0s";
-    main.style.transform="scale(0.75)"
-    about.style.transitionDelay=".3s";
-    about.style.transform="translateX(0%)"
+const showAbout = function () {
+
+    main.style.transitionDelay = "0s";
+    main.style.transform = "scale(0.75)";
+    about.style.transitionDelay = ".3s";
+    about.style.transform = "scale(1)"
 };
 
-const hidePortfolio = function() {
-    main.style.transitionDelay=".3s";
-    main.style.transform="scale(1)"
-    portfolio.style.transitionDelay="0s";
-    portfolio.style.transform="translateX(-100%)"
+const hidePortfolio = function () {
+    main.style.transitionDelay = ".3s";
+    main.style.transform = "scale(1)"
+    portfolio.style.transitionDelay = "0s";
+    portfolio.style.transform = "scale(0)"
 };
-const hideAbout = function() {
+const hideAbout = function () {
 
-    main.style.transitionDelay=".3s";
-    main.style.transform="scale(1)";
-    about.style.transitionDelay="0s";
-    about.style.transform="translateX(-100%)";
+    main.style.transitionDelay = ".3s";
+    main.style.transform = "scale(1)";
+    about.style.transitionDelay = "0s";
+    about.style.transform = "scale(0)";
 };
 
 openPortfolio.addEventListener("click", showPortfolio);
