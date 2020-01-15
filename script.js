33b31c2fb1419687025e4be5006aeef619359a2d
aronzych <57272508+aronzych@users.noreply.github.com>
2020-01-16 00:50:27 +0100

    Add new transition, fix media , animation gallery

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const main = document.querySelector(".main");
 const about = document.querySelector(".about");
 const portfolio = document.querySelector(".portfolio");
 const gallery = document.querySelector(".gallery");
-
+const img = document.querySelectorAll(".gallery__img");
 
 const openPortfolio = document.querySelector(".main__button--openPortfolio");
 const openAbout = document.querySelector(".main__button--openAbout");
@@ -14,46 +14,57 @@ const closeGallery = document.querySelector(".gallery__button");
 
 const showPortfolio = function () {
     main.style.transitionDelay = "0s";
-    main.style.transform = "scale(0.75)";
+    main.style.transform = "rotateX(10deg)";
     portfolio.style.transitionDelay = ".3s";
-    portfolio.style.transform = "scale(1)";
+    portfolio.style.transform = "translateY(0)";
 };
 const showAbout = function () {
 
     main.style.transitionDelay = "0s";
-    main.style.transform = "scale(0.75)";
+    main.style.transform = "rotateX(10deg)";
     about.style.transitionDelay = ".3s";
-    about.style.transform = "scale(1)"
+    about.style.transform = "translateY(0)"
 };
 
 const showGallery = function () {
 
     // portfolio.style.transitionDelay = "0s";
     // portfolio.style.transform = "scale(0.75)";
-    gallery.style.transitionDelay = ".3s";
-    gallery.style.transform = "scale(1)"
+    // gallery.style.transitionDelay = ".3s";
+    gallery.style.transform = "translateY(0)";
+    let delay = 0;
+    img.forEach((el) => {
+        delay += 0.1;
+        el.style.transitionDelay = `${delay}s`;
+        console.log(`${delay} s`);
+        el.style.opacity = "1"
+    })
 };
 
 const hidePortfolio = function () {
     main.style.transitionDelay = ".3s";
-    main.style.transform = "scale(1)"
+    main.style.transform = "rotateX(0)";
     portfolio.style.transitionDelay = "0s";
-    portfolio.style.transform = "scale(0)"
+    portfolio.style.transform = "translateY(-100%)";
 };
 const hideAbout = function () {
 
     main.style.transitionDelay = ".3s";
-    main.style.transform = "scale(1)";
+    main.style.transform = "rotateX(0)";
     about.style.transitionDelay = "0s";
-    about.style.transform = "scale(0)";
+    about.style.transform = "translateY(-100%)";
 };
 
 const hideGallery = function () {
 
     // portfolio.style.transitionDelay = ".3s";
     // portfolio.style.transform = "scale(1)";
-    gallery.style.transitionDelay = "0s";
-    gallery.style.transform = "scale(0)";
+    // gallery.style.transitionDelay = "0s";
+    gallery.style.transform = "translateY(-100%)";
+    img.forEach((el) => {
+        el.style.transitionDelay = "0s";
+        el.style.opacity = "0";
+    })
 };
 
 openPortfolio.addEventListener("click", showPortfolio);
